abc99659c3e12cebd92c82ee6396b490ade65201
Francis Badasu <nytro04@gmail.com>
2020-01-01 20:11:59 +0000

    navbar refactored

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,35 @@
-
-import React from 'react';
-import {NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return(
-    <div class="pos-f-t">
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class=" box p-4">
-            <NavLink to='/' class="around">Home</NavLink>
-            <NavLink to='/' class="around">Profile</NavLink>
-            <NavLink to='/' class="around">Calculator</NavLink>
-            </div>
-        </div>
-        <nav class="navbar navbar-dark box1">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-             </button>
-        </nav>
-   </div>
-    )
-}
+  return (
+    <nav id="navbar" className="navbar navbar-expand-sm navbar-dark box1">
+      <NavLink to="/" class="navbar-brand around">
+        Name
+      </NavLink>
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#portfolio"
+        aria-controls="portfolio"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="portfolio">
+        <ul className="navbar-nav ml-auto mt-2 mr-4">
+          <li className="nav-item mr-3">
+            <NavLink to="/" class="nav-link around">
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 export default Header;
