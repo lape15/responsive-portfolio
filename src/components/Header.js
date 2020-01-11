e1757e5e6bc4850f0e954fc4e8b441506697b7c2
Francis Badasu <nytro04@gmail.com>
2020-01-11 13:07:41 +0000

    header redesgin

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,44 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav id="navbar" className="navbar navbar-expand-sm navbar-dark box1">
-      <NavLink to="/" class="navbar-brand around">
-        Name
-      </NavLink>
-
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#portfolio"
-        aria-controls="portfolio"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="portfolio">
-        <ul className="navbar-nav ml-auto mt-2 mr-4">
-          <li className="nav-item mr-3">
-            <NavLink to="/" class="nav-link around">
-              Home
+    <div className="main-nav">
+      <div className="row">
+        <div className="col-md-6">
+          <nav
+            id="navbar"
+            className="navbar navbar-expand-sm navbar-light bg-light"
+          >
+            <NavLink to="/" class="navbar-brand around">
+              Name
             </NavLink>
-          </li>
-        </ul>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#portfolio"
+              aria-controls="portfolio"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="portfolio">
+              <ul className="navbar-nav ml-auto mt-2 mr-4">
+                <li className="nav-item mr-3">
+                  <NavLink to="/" class="nav-link around">
+                    Home
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-6 main-nav__nav-2">
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
+import profile from "./../assets/lape.jpeg";
 
-const Home = () => 
-    (
-        <div class="container">
-        <div class="row">
-          <div class="col">
-          <h2>Hello</h2>
-          <p>Whats going on</p>
-          </div>
-          
+const Home = () => {
+  return (
+    <div className="header">
+      <div className="row">
+        <div className="col-md-6 header__col-1">Home stuff here</div>
+        <div className="col-md-6 header__col-2">
+          <img src={profile} alt="profile" className="img-fluid" />
         </div>
-        </div>
-    )
-
+      </div>
+    </div>
+  );
+};
 
 export default Home;
